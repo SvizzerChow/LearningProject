fd013a36f3e52fe60ab7d994cab4d7edb46ef6a5
zhoushuai <zhoushuai@souche.com>
2019-11-28 18:26:04 +0800

    @add 构建版本

--- src/main/java/ink/chow/learning/service/ThreadPool.java
+++ src/main/java/ink/chow/learning/service/ThreadPool.java
@@ -4,8 +4,24 @@ import java.util.concurrent.Executor;
 
 public class ThreadPool {
 
+    private static final int COUNT_BITS = Integer.SIZE - 3;
+    private static final int COUNT_MASK = (1 << COUNT_BITS) - 1;
+
+    // runState is stored in the high-order bits
+    private static final int RUNNING    = -1 << COUNT_BITS;
+    private static final int SHUTDOWN   =  0 << COUNT_BITS;
+    private static final int STOP       =  1 << COUNT_BITS;
+    private static final int TIDYING    =  2 << COUNT_BITS;
+    private static final int TERMINATED =  3 << COUNT_BITS;
+
     public static void main(String[] args) {
-        Executor
+        System.out.println(COUNT_BITS);
+        System.out.println(COUNT_MASK);
+        System.out.println(RUNNING);
+        System.out.println(SHUTDOWN);
+        System.out.println(STOP);
+        System.out.println(TIDYING);
+        System.out.println(TERMINATED);
 
 
     }
