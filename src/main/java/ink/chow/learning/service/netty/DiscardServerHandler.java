95a439853e900f34a98dd913b4d48b353fbf98f9
zhoushuai <zhoushuai@souche.com>
2019-12-04 20:08:01 +0800

    @update netty server read

--- src/main/java/ink/chow/learning/service/netty/DiscardServerHandler.java
+++ src/main/java/ink/chow/learning/service/netty/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package ink.chow.learning.service.netty;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Description:
@@ -14,7 +15,15 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()){
+                System.out.println((char)in.readByte());
+                System.out.flush();
+            }
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override
