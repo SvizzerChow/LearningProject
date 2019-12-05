9d8ed0108e0a429a9fe827f9fae0b03c21d48fb6
zhoushuai <zhoushuai@souche.com>
2019-12-05 20:11:29 +0800

    @update netty 聊天

--- src/main/java/ink/chow/learning/service/netty/DiscardServerHandler.java
+++ src/main/java/ink/chow/learning/service/netty/DiscardServerHandler.java
@@ -1,8 +1,15 @@
 package ink.chow.learning.service.netty;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
@@ -19,17 +26,24 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf in = (ByteBuf) msg;
         try {
-            StringBuffer stringBuffer = new StringBuffer();
+            byte[] b = new byte[1024];
+            int size = 0;
             while (in.isReadable()){
-                stringBuffer.append((char)in.readByte());
+                if (size == b.length){
+                    b = Arrays.copyOf(b, b.length*2);
+                }
+                b[size++] = in.readByte();
             }
-            String str = stringBuffer.toString();
+            String str = new String(b, 0, size);
+            System.out.println(str);
+            final ByteBuf byteBuf = ctx.alloc().buffer(4);
             if ("hello".equals(str)){
-                ctx.write("你好,干嘛");
+                byteBuf.writeCharSequence("你好", StandardCharsets.UTF_8);
             }
-            if ("时间".equals(str)){
-                ctx.write(System.currentTimeMillis());
+            if ("time".equals(str)){
+                byteBuf.writeCharSequence(new Date().toString(), StandardCharsets.UTF_8);
             }
+            final ChannelFuture f = ctx.writeAndFlush(byteBuf);
         }finally {
             ReferenceCountUtil.release(msg);
         }
